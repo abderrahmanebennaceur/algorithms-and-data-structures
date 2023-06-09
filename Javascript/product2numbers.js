e2e108f50d599875093b45faff0118e9135ccc2e
abderrahmane <abderrahmane.bennaceur.213@gmail.com>
2023-06-09 19:52:04 +0100

    product of two numbers

---
+++ Javascript/product2numbers.js
@@ -0,0 +1,12 @@
+// Write a program to find the product of two numbers.
+
+function product2numbers(a, b) {
+  return a * b;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = product2numbers(number1, number2);
+
+console.log(`The product of ${number1} and ${number2} is ${result}`);
