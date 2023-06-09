307523cc4c4c162152f981ba5025e39845432bc8
abderrahmane <abderrahmane.bennaceur.213@gmail.com>
2023-06-09 20:05:21 +0100

    quotient of two numbers

---
+++ Javascript/quotient2numbers.js
@@ -0,0 +1,12 @@
+// Write a program to find the quotient of two numbers.
+
+function quotient2numbers(a, b) {
+  return a / b;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = quotient2numbers(number1, number2);
+
+console.log(`The quotient of ${number1} and ${number2} is ${result}`);
