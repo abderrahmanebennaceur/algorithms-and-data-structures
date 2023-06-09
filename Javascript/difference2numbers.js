6b952bad9b276a95ffc9762050088c6a7ec1363a
abderrahmane <abderrahmane.bennaceur.213@gmail.com>
2023-06-09 19:50:38 +0100

    difference between two numbers

---
+++ Javascript/difference2numbers.js
@@ -0,0 +1,12 @@
+// Write a program to find the difference between two numbers.
+
+function difference2numbers(a, b) {
+  return a - b;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = difference2numbers(number1, number2);
+
+console.log(`The difference between ${number1} and ${number2} is ${result}`);
