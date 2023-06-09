132a50dd93ceddbc2e6b96a420ee5f2e5e8b745e
abderrahmane <abderrahmane.bennaceur.213@gmail.com>
2023-06-09 19:47:55 +0100

    sum of two numbers

---
+++ Javascript/sum2numbers.js
@@ -0,0 +1,12 @@
+// Write a program to find the sum of two numbers.
+
+function product2numbers(a, b) {
+  return a + b;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const sum = product2numbers(number1, number2);
+
+console.log(`The sum of ${number1} and ${number2} is ${sum}`);
